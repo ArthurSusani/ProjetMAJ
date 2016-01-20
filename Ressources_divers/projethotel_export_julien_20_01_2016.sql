0ce4e640504d025ce50f8ff167419a5029958ac9
jul54 <int0x13@gmail.com>
2016-01-20 16:20:37 +0100

    base SQL faite avec schema UML et plan avec homestyler étage 1

---
+++ Ressources_divers/projethotel_export_julien_20_01_2016.sql
@@ -0,0 +1,178 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.12
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 20 Janvier 2016 à 16:17
+-- Version du serveur :  5.6.25
+-- Version de PHP :  5.6.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `projethotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `booking`
+--
+
+CREATE TABLE IF NOT EXISTS `booking` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_room` int(11) NOT NULL,
+  `begin` date NOT NULL,
+  `end` date NOT NULL,
+  `validate` date NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL,
+  `title` enum('Mr','Mme','','') DEFAULT 'Mr',
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='stoque les messages reçus par les visiteurs envoyés a l''admin';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `logs`
+--
+
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `adressClient` varchar(100) NOT NULL,
+  `postcodeClient` varchar(5) NOT NULL,
+  `cityClient` varchar(30) NOT NULL,
+  `telephone` varchar(10) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `birthday` date NOT NULL,
+  `role` enum('admin','user','','') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `logs`
+--
+
+INSERT INTO `logs` (`id`, `firstname`, `lastname`, `adressClient`, `postcodeClient`, `cityClient`, `telephone`, `mail`, `password`, `birthday`, `role`) VALUES
+(11, 'julien', 'baudouin', '15, rue Henri dunant', '54150', 'briey', '', 'baudouinjulien@free.fr', '$2y$10$rO3ZZApA.XTaXxOSqLag3eLjNkJZinyH8NlPTbTJwyyMwSdq7Vtw.', '1983-01-03', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `opinion`
+--
+
+CREATE TABLE IF NOT EXISTS `opinion` (
+  `id` int(11) NOT NULL,
+  `id_booking` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `date_opinion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stoque les avis des clients sur un séjour';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rooms`
+--
+
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `floor` int(11) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `singlebed` int(11) NOT NULL,
+  `doublebed` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `opinion`
+--
+ALTER TABLE `opinion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT pour la table `opinion`
+--
+ALTER TABLE `opinion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
