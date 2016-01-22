fb770d7806ef1f762b4a52232889f1cdc935ce21
Mylene <mk@bwets.net>
2016-01-22 17:02:06 +0100

    contact + beta view

--- app/templates/contact/viewcontact.php
+++ app/templates/contact/viewcontact.php
@@ -1,6 +1,12 @@
 <?php $this->layout('layout', ['title' => 'Bureau des réclamations']) ?>
 
 <?php $this->start('main_content') ?>
-	
 
+	<ul>
+		<?php foreach ($viewall as $view) : ?>		
+			<li><a href="<?= $this->url('contact/details', ['id' => $view['id']]) ?>">
+			<?= $view['subject'] ?></a></li>
+		<?php endforeach ?>
+	<ul>
+	
 <?php $this->stop('main_content') ?>
